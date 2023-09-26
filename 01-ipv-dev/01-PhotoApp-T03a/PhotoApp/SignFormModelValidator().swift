9f4b055c85fc29fda86c2e8aaf21f0265bb8ba71
ignasiperez <ignasiperez@gmail.com>
2023-09-26 12:32:50 +0200

    Task02: Test for a very short First Name

---
+++ 01-ipv-dev/01-PhotoApp-T03a/PhotoApp/SignFormModelValidator().swift
@@ -0,0 +1,26 @@
+//
+//  SignFormModelValidator().swift
+//  PhotoApp
+//
+//  Created by Ignasi Perez-Valls on 10/9/23.
+//
+
+import Foundation
+
+class SignFormModelValidator {
+    
+    func isFirstNameValid(firstName: String) -> Bool {
+        var returnValue = true
+        
+//      if firstName.isEmpty {
+//          returnValue = false
+//      }
+        
+        if firstName.count < 2  {
+            returnValue = false
+        }
+        
+        return returnValue
+    } // isFirstNameValid(firstName:)
+    
+} // SignFormModelValidator
---
+++ 01-ipv-dev/01-PhotoApp-T03b/PhotoApp/SignupFormModelValidator.swift
@@ -0,0 +1,22 @@
+//
+//  SignFormModelValidator().swift
+//  PhotoApp
+//
+//  Created by Ignasi Perez-Valls on 10/9/23.
+//
+
+import Foundation
+
+class SignupFormModelValidator {
+    
+    func isFirstNameValid(firstName: String) -> Bool {
+        var returnValue = true
+        
+        if firstName.count < 2  {
+            returnValue = false
+        }
+        
+        return returnValue
+    } // isFirstNameValid(firstName:)
+    
+} // SignFormModelValidator
