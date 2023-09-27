0b9ba077e8522f2632fd5f25f868d414402aba6c
ignasiperez <ignasiperez@gmail.com>
2023-09-27 08:04:46 +0200

    Task03: Test_for_a_very_short_First_Name

---
+++ 01-ipv-dev/01-PhotoApp-T03b-Test_for_a_very_short_First_Name-Refactoring/PhotoAppTests/SignupFormModelValidatorTests.swift
@@ -0,0 +1,47 @@
+//
+//  SignupFormModelValidatorTests.swift
+//  PhotoAppTests
+//
+//  Created by Ignasi Perez-Valls on 10/9/23.
+//
+
+import XCTest
+@testable import PhotoApp
+
+final class SignupFormModelValidatorTests: XCTestCase {
+    
+    var sut: SignupFormModelValidator!
+    
+    override func setUp() {
+        sut = SignupFormModelValidator()
+    }
+    
+    override func tearDown() {
+        sut = nil
+    }
+    
+    func test_SignupFormModelValidator_WhenValidFirstNameProvided_ShouldReturnTrue() {
+        // GIVEN
+//      let sut = SignFormModelValidator()
+        
+        // WHEN
+        let isFirstNameValid = sut.isFirstNameValid(firstName: "Ignasi")
+        
+        // THEN
+        XCTAssertTrue(isFirstNameValid,
+                      "The isFirstNameValid() should have returned TRUE for a valid first name")
+    } // test_SignFormModelValidator_whenValidFirstNameProvided_shouldReturnTrue()
+    
+    func test_SignupFormModelValidator_WhenTooShortFirstNameProvided_ShouldReturnFalse() {
+        // GIVEN
+//      let sut = SignupFormModelValidator()
+        
+        // WHEN
+        let isFirstNameValid = sut.isFirstNameValid(firstName: "I")
+        
+        // THEN
+        XCTAssertFalse(isFirstNameValid,
+                       "The isFirstNameValid() should have returned FALSE for a first name shorter than two two characters")
+    } // test_SignFormModelValidator_whenTooShortFirstNameProvided_shouldReturnFalse()
+    
+} // SignupFormModelValidatorTests
