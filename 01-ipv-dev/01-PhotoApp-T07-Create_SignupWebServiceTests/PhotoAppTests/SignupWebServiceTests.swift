950f3e540569d6a135dedad710e302de66325c84
ignasiperez <ignasiperez@gmail.com>
2023-10-03 13:27:16 +0200

    Task07: Create SignupWebServiceTests

---
+++ 01-ipv-dev/01-PhotoApp-T07-Create_SignupWebServiceTests/PhotoAppTests/SignupWebServiceTests.swift
@@ -0,0 +1,20 @@
+//
+//  SignupWebServiceTests.swift
+//  PhotoAppTests
+//
+//  Created by Ignasi Perez-Valls on 3/10/23.
+//
+
+import XCTest
+
+final class SignupWebServiceTests: XCTestCase {
+
+    override func setUp() {
+        
+    }
+
+    override func tearDown()  {
+    
+    }
+
+} // SignupWebServiceTests
