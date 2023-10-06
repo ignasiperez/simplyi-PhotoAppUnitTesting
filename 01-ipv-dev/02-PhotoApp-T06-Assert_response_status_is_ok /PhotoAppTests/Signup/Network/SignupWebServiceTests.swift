21604690b9a141e0b306a0e711b81e841d860a91
ignasiperez <ignasiperez@gmail.com>
2023-10-06 13:51:16 +0200

    Assert response status is 'ok'

---
+++ 01-ipv-dev/02-PhotoApp-T06-Assert_response_status_is_ok /PhotoAppTests/Signup/Network/SignupWebServiceTests.swift
@@ -0,0 +1,42 @@
+//
+//  SignupWebServiceTests.swift
+//  PhotoAppTests
+//
+//  Created by Ignasi Perez-Valls on 3/10/23.
+//
+
+import XCTest
+@testable import PhotoApp
+
+final class SignupWebServiceTests: XCTestCase {
+
+    override func setUp() {
+        
+    }
+
+    override func tearDown()  {
+    
+    }
+    
+    func test_SignupWebService_WhenGivenSuccessFullResponse_ReturnsSuccess() {
+        // GIVEN
+        let sut = SignupWebService()
+        
+        let signupFormRequestModel = SignupFormRequestModel(
+            firstName: "Ignasi",
+            lastName: "Perez",
+            email: "test@test.com",
+            password: "12345678"
+        )
+        
+        // WHEN
+        sut.signup(withForm: signupFormRequestModel) { 
+            (signupResponseModel, error) in
+            // THEN
+            // "{\"status\":\"ok\"}"
+            XCTAssertEqual(signupResponseModel?.status, "ok")
+        }
+        
+    } // test_SignupWebService_WhenGivenSuccessFullResponse_ReturnsSuccess()
+
+} // SignupWebServiceTests
