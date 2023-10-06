48285e9ed5459e2199e23dd87a008b64ca95c7fe
ignasiperez <ignasiperez@gmail.com>
2023-10-06 11:35:49 +0200

    Task02: Create SignupWebService

---
+++ 01-ipv-dev/02-PhotoApp-T02-Create_SignupWebService/PhotoApp/SignupWebService.swift
@@ -0,0 +1,12 @@
+//
+//  SignupWebService.swift
+//  PhotoApp
+//
+//  Created by Ignasi Perez-Valls on 3/10/23.
+//
+
+import Foundation
+
+class SignupWebService {
+    
+}
---
+++ 01-ipv-dev/02-PhotoApp-T02-Create_SignupWebService/PhotoAppTests/SignupWebServiceTests.swift
@@ -0,0 +1,27 @@
+//
+//  SignupWebServiceTests.swift
+//  PhotoAppTests
+//
+//  Created by Ignasi Perez-Valls on 3/10/23.
+//
+
+import XCTest
+@testable import PhotoApp
+
+final class SignupWebServiceTests: XCTestCase {
+
+    override func setUp() {
+        
+    }
+
+    override func tearDown()  {
+    
+    }
+    
+    func test_SignupWebService_WhenGivensuccessFullResponse_ReturnsSuccess() {
+        // GIVEN
+        let sut = SignupWebService()
+    }
+
+
+} // SignupWebServiceTests
